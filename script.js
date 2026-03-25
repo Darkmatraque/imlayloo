c647212401444106e13d862f4a2fdeec3b2b70e8
Darkmatraque <kevin2007baumann@gmail.com>
2026-03-25 16:30:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn = document.querySelector('.settings-button');
     const menu = document.querySelector('.settings-menu');
 
-    if (!btn || !menu) return; // Sécurité si un élément manque
+    if (!btn || !menu) return;
 
     btn.addEventListener('click', () => {
         menu.classList.toggle('open');

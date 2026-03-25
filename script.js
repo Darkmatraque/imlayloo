b1c104603e585d4a18c1819bd9bb1469a7017785
Darkmatraque <kevin2007baumann@gmail.com>
2026-03-25 19:33:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle('open');
     });
 });
+
+// ----- MODE SOMBRE / CLAIR -----
+
+const modeButton = document.querySelector(".settings-menu a:nth-child(2)");
+
+// Charger le mode sauvegardé
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
+// Clic sur "Mode"
+modeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    // Sauvegarde
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
